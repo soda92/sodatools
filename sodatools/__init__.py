12bb7b29432dfd55c9d2014575f8caeb9b0bc147
sodacris <wjxa20152015@gmail.com>
2025-02-16 08:59:47 +0800

    add get_glob_files

--- sodatools/__init__.py
+++ sodatools/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from contextlib import contextmanager
 import os
+import glob
 
 
 @contextmanager
@@ -15,12 +16,24 @@ def read_path(p: Path) -> str:
     return p.read_text(encoding="utf8")
 
 
-def write_path(p: Path, content: str):
+def write_path(p: Path, content: str) -> None:
     p.write_text(content, encoding="utf8")
 
 
-def str_path(p: Path):
+def str_path(p: Path) -> str:
     return str(p).replace("\\", "/")
 
 
-__all__ = ["read_path", "write_path", "str_path", "CD"]
+def get_glob_files(*patterns, recursive=True, root_dir=".") -> list[Path]:
+    ret = []
+    if root_dir == ".":
+        root_dir = os.getcwd()
+    root_dir = Path(root_dir).resolve()
+    for pattern in patterns:
+        files = glob.glob(pattern, recursive=True, root_dir=str_path(root_dir))
+        files = map(root_dir.joinpath, files)
+        ret.extend(files)
+    return ret
+
+
+__all__ = ["read_path", "write_path", "str_path", "CD", "get_glob_files"]
